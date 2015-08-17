9f83f5a02b40ce931e099d899a6681829ef3169a
gfaust-qb <guido.faust@questback.com>
2015-08-18 01:08:34 +0200

    Added PHPUnit and first Test.

--- tests/XMLSecEncTest.php
+++ tests/XMLSecEncTest.php
@@ -4,13 +4,14 @@ namespace RobRichards\XMLSecLibs;
 
 use PHPUnit_Framework_TestCase;
 
-require_once '../../xmlseclibs.php';
+require_once __DIR__ . '/../xmlseclibs.php';
 
 
 class XMLSecEncTest extends PHPUnit_Framework_TestCase
 {
     public function testSetNode()
     {
+        echo __DIR__;
         $xmlSecEnc = new XMLSecEnc();
         $domNodeGiven = new \DOMNode();
         $domNodeExpected = $domNodeGiven;
