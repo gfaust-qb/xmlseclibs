4b5d5dc07cae6aa96d74f864c0e45c65d540d7e5
gfaust-qb <guido.faust@questback.com>
2015-08-19 01:31:28 +0200

    Added RetrievalmethodFindkeyTest.

--- tests/EncrypteddataNodeOrderTest.php
+++ tests/EncrypteddataNodeOrderTest.php
@@ -1,9 +1,9 @@
 <?php
 namespace RobRichards\XMLSecLibs;
 
-use PHPUnit_Framework_TestCase;
 use DOMDocument;
 use DOMElement;
+use PHPUnit_Framework_TestCase;
 
 require_once __DIR__ . '/../xmlseclibs.php';
 
@@ -11,53 +11,51 @@ class EncrypteddataNodeOrderTest extends PHPUnit_Framework_TestCase {
 
     public function testBasicDoc() {
 
-        echo __DIR__;
+        $dom = new DOMDocument();
+        $dom->load(__DIR__ . '/basic-doc.xml');
 
-            $dom = new DOMDocument();
-            $dom->load(__DIR__ . '/basic-doc.xml');
+        $objKey = new XMLSecurityKey(XMLSecurityKey::AES256_CBC);
+        $objKey->generateSessionKey();
 
-            $objKey = new XMLSecurityKey(XMLSecurityKey::AES256_CBC);
-            $objKey->generateSessionKey();
+        $siteKey = new XMLSecurityKey(XMLSecurityKey::RSA_OAEP_MGF1P, array('type'=>'public'));
+        $siteKey->loadKey(__DIR__ . '/mycert.pem', TRUE, TRUE);
 
-            $siteKey = new XMLSecurityKey(XMLSecurityKey::RSA_OAEP_MGF1P, array('type'=>'public'));
-            $siteKey->loadKey(__DIR__ . '/mycert.pem', TRUE, TRUE);
+        $enc = new XMLSecEnc();
+        $enc->setNode($dom->documentElement);
+        $enc->encryptKey($siteKey, $objKey);
 
-            $enc = new XMLSecEnc();
-            $enc->setNode($dom->documentElement);
-            $enc->encryptKey($siteKey, $objKey);
+        $enc->type = XMLSecEnc::Content;
+        $encNode = $enc->encryptNode($objKey);
 
-            $enc->type = XMLSecEnc::Content;
-            $encNode = $enc->encryptNode($objKey);
+        $nodeOrder = array(
+            'EncryptionMethod',
+            'KeyInfo',
+            'CipherData',
+            'EncryptionProperties',
+        );
 
-            $nodeOrder = array(
-                'EncryptionMethod',
-                'KeyInfo',
-                'CipherData',
-                'EncryptionProperties',
-            );
-
-            $prevNode = 0;
-            for ($node = $encNode->firstChild; $node !== NULL; $node = $node->nextSibling) {
-                if (! ($node instanceof DOMElement)) {
-                    /* Skip comment and text nodes. */
-                    continue;
-                }
-
-                $name = $node->localName;
-
-                $cIndex = array_search($name, $nodeOrder, TRUE);
-                $this->assertNotFalse($cIndex, "Unknown node: $name");
-
-                if ($cIndex >= $prevNode) {
-                    /* In correct order. */
-                    $prevNode = $cIndex;
-                    continue;
-                }
-                $prevName = $nodeOrder[$prevNode];
-                $this->assertGreaterThanOrEqual($cIndex, $prevNode, "Incorrect order: $name must appear before $prevName");
+        $prevNode = 0;
+        for ($node = $encNode->firstChild; $node !== NULL; $node = $node->nextSibling) {
+            if (! ($node instanceof DOMElement)) {
+                /* Skip comment and text nodes. */
+                continue;
             }
 
-            $this->assertTrue(true, 'OK');
+            $name = $node->localName;
+
+            $cIndex = array_search($name, $nodeOrder, TRUE);
+            $this->assertNotFalse($cIndex, "Unknown node: $name");
+
+            if ($cIndex >= $prevNode) {
+                /* In correct order. */
+                $prevNode = $cIndex;
+                continue;
+            }
+            $prevName = $nodeOrder[$prevNode];
+            $this->assertGreaterThanOrEqual($cIndex, $prevNode, "Incorrect order: $name must appear before $prevName");
         }
+
+        $this->assertTrue(true, 'OK');
+    }
 }
  
---
+++ tests/SignEmptyUriTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: gfaust
+ * Date: 19.08.2015
+ * Time: 01:00
+ */
+
+namespace RobRichards\XMLSecLibs;
+
+
+class SignEmptyUriTest extends \PHPUnit_Framework_TestCase
+{
+
+}
