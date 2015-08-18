958285bdcaa392e6ed37b4ad9fbce2f4ac000b87
gfaust-qb <guido.faust@questback.com>
2015-08-18 18:15:20 +0200

    Add some Tests;

---
+++ tests/GetcipherdataTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace RobRichards\XMLSecLibs;
+
+use DOMDocument;
+use PHPUnit_Framework_TestCase;
+
+require_once(dirname(__FILE__) . '/../xmlseclibs.php');
+
+
+class GetcipherdataTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Test the getCipherData() function.
+     */
+    public function test()
+    {
+        $dataCipherValueExpected = 'e3b188c5a139655d14d3f7a1e6477bc3';
+        $keyCipherValueExpected  = 'b36f81645cb068dd59d69c7ff96e835a';
+
+        $doc = new DOMDocument();
+        $doc->load(__DIR__ . '/oaep_sha1-res.xml');
+
+        $objenc = new XMLSecEnc();
+        $encData = $objenc->locateEncryptedData($doc);
+        $objenc->setNode($encData);
+
+        $ciphervalue = $objenc->getCipherValue();
+        $this->assertEquals($dataCipherValueExpected, md5($ciphervalue), 'Data CipherValue');
+
+        $objKey = $objenc->locateKey();
+        $objKeyInfo = $objenc->locateKeyInfo($objKey);
+        $encryptedKey = $objKeyInfo->encryptedCtx;
+
+        $keyCV = $encryptedKey->getCipherValue();
+        $this->assertEquals($keyCipherValueExpected, md5($keyCV), "Key CipherValue");
+
+    }
+}
+ 
