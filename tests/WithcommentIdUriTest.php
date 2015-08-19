f983260267f05d22a0155b015d9bcd42fcbc88fc
gfaust-qb <guido.faust@questback.com>
2015-08-19 10:04:42 +0200

    Add some Tests;

---
+++ tests/WithcommentIdUriTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace RobRichards\XMLSecLibs;
+
+use PHPUnit_Framework_TestCase;
+use DOMDocument;
+
+require_once(__DIR__ . '/../xmlseclibs.php');
+
+class WithcommentIdUriTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * WithComments with an ID reference.
+     * @description Checks that comments are removed when using an ID URI in a Reference.
+     */
+    public function test()
+    {
+        $doc = new DOMDocument();
+        $doc->load(__DIR__ . '/withcomment-id-uri.xml');
+        $objXMLSecDSig = new XMLSecurityDSig();
+        $objXMLSecDSig->idKeys = array('xml:id');
+
+        $objDSig = $objXMLSecDSig->locateSignature($doc);
+
+        $this->assertNotNull($objDSig, 'Locate Signature');
+        $this->assertInstanceOf('DOMNode', $objDSig, '$objDSig instanceof DOMNode');
+
+        $retVal = $objXMLSecDSig->validateReference();
+        /*
+         * Since we are testing reference canonicalization, we don't need to
+         * do more than reference validation here.
+         */
+        $this->assertTrue($retVal, 'Reference');
+    }
+
+}
+ 
